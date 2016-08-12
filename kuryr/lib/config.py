3aff29737c5eb00a613187de047186fda5028235
Vikas Choudhary <choudharyvikas16@gmail.com>
2016-08-12 11:17:21 +0530

    Uncomment registration of config core opts

--- kuryr/lib/config.py
+++ kuryr/lib/config.py
@@ -102,7 +102,7 @@ binding_opts = [
 
 
 CONF = cfg.CONF
-#CONF.register_opts(core_opts)
+CONF.register_opts(core_opts)
 CONF.register_opts(neutron_opts, group='neutron_client')
 CONF.register_opts(keystone_opts, group='keystone_client')
 CONF.register_opts(binding_opts, 'binding')
